a25b5aaff73be8ec4be5cefe90b7d839aa05de1e
Nirmalmahesh S <nirmalamhesh54@gmail.com>
2019-03-01 22:03:48 +0530

    Basic Hibernate With get , Save and hibernate.cfg.xml file

---
+++ FirstHibernateProject/src/main/java/FirstHibernateProject/FirstHibernateProject/Alien.java
@@ -0,0 +1,56 @@
+package FirstHibernateProject.FirstHibernateProject;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Alien {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	int aid;
+	Name name;
+	String acolor;
+	public int getAid() {
+		return aid;
+	}
+	public void setAid(int aid) {
+		this.aid = aid;
+	}
+	
+	public String getAcolor() {
+		return acolor;
+	}
+	public void setAcolor(String acolor) {
+		this.acolor = acolor;
+	}
+	public Name getName() {
+		return name;
+	}
+	public void setName(Name name) {
+		this.name = name;
+	}
+	public Alien(int aid, Name name, String acolor) {
+		super();
+		this.aid = aid;
+		this.name = name;
+		this.acolor = acolor;
+	}
+	
+	public Alien(Name name, String acolor) {
+		super();
+		this.name = name;
+		this.acolor = acolor;
+	}
+	public Alien() {
+		super();
+	}
+	@Override
+	public String toString() {
+		return "Alien [aid=" + aid + ", name=" + name + ", acolor=" + acolor + "]";
+	}
+	
+
+}
---
+++ FirstHibernateProject/src/main/java/FirstHibernateProject/FirstHibernateProject/App.java
@@ -0,0 +1,46 @@
+package FirstHibernateProject.FirstHibernateProject;
+
+import org.hibernate.Session;
+import org.hibernate.SessionBuilder;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	/*Name n = new Name("Nirmalmahesh"," ","Subramani");
+    	
+        Alien a1 = new Alien(n,"Gray");*/
+    	
+    	Alien a1;
+    	
+    	
+
+        
+      /*  Configuration cf = new Configuration().configure().addAnnotatedClass(Alien.class);
+        SessionFactory sf = cf.buildSessionFactory();
+        Session session = sf.getCurrentSession();
+*/
+        Session session = new Configuration().configure()
+        		.addAnnotatedClass(Alien.class)
+        		.buildSessionFactory().openSession();
+        
+        Transaction tx = session.beginTransaction();
+        
+        //session.save(a1);
+       
+        a1 = (Alien) session.get(Alien.class, 1);
+        
+        System.out.println(a1);
+        tx.commit();
+        
+        session.close();
+        
+    }
+}
