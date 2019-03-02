fa262b38914910d8f3d126596e049537f4c72487
Nirmalmahesh S <nirmalamhesh54@gmail.com>
2019-03-02 17:39:23 +0530

    Deliting Old Data

--- FirstHibernateProject/src/main/java/FirstHibernateProject/FirstHibernateProject/App.java
+++ FirstHibernateProject/src/main/java/FirstHibernateProject/FirstHibernateProject/App.java
@@ -13,31 +13,24 @@ import org.hibernate.cfg.Configuration;
 public class App 
 {
     public static void main( String[] args )
-    {
-    	/*Name n = new Name("Nirmalmahesh"," ","Subramani");
-    	
-        Alien a1 = new Alien(n,"Gray");*//**/
-    	
-    	Alien a1;
-    	
-    	
+    {  	
+    	   	
 
         
       /*  Configuration cf = new Configuration().configure().addAnnotatedClass(Alien.class);
         SessionFactory sf = cf.buildSessionFactory();
         Session session = sf.getCurrentSession();
-*/
+        */
         Session session = new Configuration().configure()
-        		.addAnnotatedClass(Alien.class)
         		.buildSessionFactory().openSession();
         
         Transaction tx = session.beginTransaction();
         
-        //session.save(a1);
+      
        
-        a1 = (Alien) session.get(Alien.class, 1);
+      
         
-        System.out.println(a1);
+      
         tx.commit();
         
         session.close();
